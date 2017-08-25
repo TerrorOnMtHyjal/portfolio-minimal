a36b9924cb7c20a4ccecc7efa76b7a8048c1a608
dustwise <jared.mohney@gmail.com>
2017-08-25 12:29:27 -0600

    top card styled

--- src/components/TopCard.js
+++ src/components/TopCard.js
@@ -16,6 +16,7 @@ const Wrap = styled.div`
   align-items: center;
   justify-content: center;
   width: 100%;
+  height: 100vh;
   color: #1a1a1d;
   background-color: #fc4445;
   font-size: 1em;
@@ -24,6 +25,7 @@ const Wrap = styled.div`
 const InnerWrap = styled.div`
   display: flex;
   flex-flow: column;
+  justify-content: center;
   width: 95%;
 `;
 
@@ -32,25 +34,24 @@ const TopInfo = styled.div`
   flex-flow: column;
   align-items: flex-end;
   width: 100%;
-  margin-bottom: 10%;
 
   & > h1 {
-    font-size: 10vw;
+    font-size: 2em;
     margin: 0;
     padding: 0;
   }
 
   & > h3 {
-    font-size: 6vw;
+    font-size: 1.25em;
     margin: 0;
     padding: 0;
-    margin-top: -0.4em;
+    margin-top: -0.3em;
     margin-bottom: 0.4em;
     color: white;
   }
 
   & > p {
-    font-size: 5vw;
+    font-size: 1em;
     color: white;
     margin: 0;
     padding: 0;
@@ -59,20 +60,25 @@ const TopInfo = styled.div`
 
 const SkillsList = styled.ul`
   display: flex;
+  flex-flow: column;
   padding: 0;
   margin: 0;
   justify-content: center;
-  flex-wrap: wrap;
+  align-items: flex-end;
   width: 100%;
   list-style-type: none;
+  color: white;
+  margin-top: 5vh;
 `;
 
 const Skill = styled.li`
   display: flex;
   justify-content: center;
   align-items: center;
+  font-size: 1em;
+  padding: 0.25em 0.5em;
   margin: 0;
-  padding: 0.5em 0.5em 0 0.5em;
+  background-color: #1a1a1d;
 `;
 
 class TopCard extends Component {
@@ -92,7 +98,6 @@ class TopCard extends Component {
             <Skill>PostgreSQL</Skill>
             <Skill>Node</Skill>
             <Skill>Express</Skill>
-            <Skill>Passport</Skill>
           </SkillsList>
         </InnerWrap>
       </Wrap>
