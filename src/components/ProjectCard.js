e7549c3688d5cc8fd08167f58f4415b21e4b6be2
dustwise <jared.mohney@gmail.com>
2017-08-28 14:43:17 -0600

    z-index issues

--- src/components/ProjectCard.js
+++ src/components/ProjectCard.js
@@ -8,39 +8,66 @@ import ProjectFeatures from './ProjectFeatures';
 
 
 const Wrap = styled.div`
+  position: relative;
   display: flex;
   flex-flow: column;
   align-items: center;
   width: 100%;
   padding-bottom: 7em;
   // background-color: ${props => props.color};
+  z-index: 1;
 `;
 
 const InnerWrap = styled.div`
+  position: relative;
   display: flex;
   flex-flow: column;
   align-items: center;
   width: 95%;
+  z-index: 1000;
+  background-color: white;
 `;
 
 const TopInfo = styled.div`
-  display: flex;
-  flex-flow: column;
-  align-items: flex-start;
+  position: relative;
   width: 100%;
   margin-bottom: 2em;
+  background-color: #3feee6;
+  z-index: 1;
 
-  & > h2 {
-    font-size: 2em;
-    margin: 0;
-    padding: 0;
+  & > div {
+    position: relative;
+    display: flex;
+    flex-flow: column;
+    align-items: flex-start;
+    z-index: 2;
+    padding: 3em 2.5%;
+
+    & > h2 {
+      font-size: 2em;
+      margin: 0;
+      padding: 0;
+    }
+
+    & > h3 {
+      font-size: 1em;
+      font-weight: 400;
+      margin: 0;
+      padding: 0;
+    }
   }
 
-  & > h3 {
-    font-size: 1em;
-    font-weight: 400;
-    margin: 0;
-    padding: 0;
+  &::before {
+    position: absolute;
+    content: '';
+    right: 0;
+    top: 0;
+    width: 1200vw;
+    height: 30vh;
+    transform: rotateZ(6deg);
+    transform-origin: 100% 50%;
+    background-color: #3feee6;
+    z-index: 1;
   }
 `;
 
@@ -50,11 +77,13 @@ class ProjectCard extends Component {
 
     return (
       <Wrap color={ backgroundColor }>
-        <InnerWrap>
-          <TopInfo>
+        <TopInfo>
+          <div>
             <h2>{ title }</h2>
             <h3>{ tagline }</h3>
-          </TopInfo>
+          </div>
+        </TopInfo>
+        <InnerWrap>
           <ImageSlider images={ images } />
           <ProjectStack stack={ stack } />
           <ProjectFeatures features={ features }/>
