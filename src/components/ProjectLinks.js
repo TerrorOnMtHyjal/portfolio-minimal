95724069b92f72753b73c66552d2ead5aaf7f02e
dustwise <jared.mohney@gmail.com>
2017-09-01 18:46:59 -0600

    buttons 3d

--- src/components/ProjectLinks.js
+++ src/components/ProjectLinks.js
@@ -5,6 +5,7 @@ const Wrap = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-around;
+  perspective: 2000px;
 
   @media screen and (min-width: 600px){
     width: 80%;
@@ -68,16 +69,68 @@ const Button = styled.a`
   }
 `;
 
+const Button3D = styled.div`
+  transform-style: preserve-3d;
+  transition: all 0.25s ease-out;
+  flex-basis: 0;
+  flex-grow: 1;
+  margin-right: 2em;
+  height: 70px;
+
+  &:hover {
+    transform: rotateX(-89deg);
+  }
+`;
+
+const ButtonFace = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: ${props => props.theme.accent};
+  font-family: 'Roboto Slab', serif;
+  height: 70px;
+  border: 2px solid ${props => props.theme.accent};
+  color: ${props => props.theme.accent};
+
+  ${props => props.default && `
+    transform: translateZ(35px);
+    background-color: transparent;
+  `}
+
+  ${props => props.active && `
+    transform: rotateX(90deg) translateZ(105px);
+    background-color: ${props => props.theme.accent};
+    color: white;
+  `}
+`;
+
 class ProjectLinks extends Component {
   render() {
     return (
       <Wrap>
-        <Button href={this.props.links.github} target="_blank" rel="noopener noreferrer">
+        {/*<Button href={this.props.links.github} target="_blank" rel="noopener noreferrer">
           <span>GITHUB</span>
         </Button>
         <Button href={this.props.links.live} target="_blank" rel="noopener noreferrer">
           <span>LIVE</span>
-        </Button>
+        </Button>*/}
+        <Button3D>
+          <ButtonFace default>
+            <span>GITHUB</span>
+          </ButtonFace>
+          <ButtonFace active>
+            <span>GITHUB</span>
+          </ButtonFace>
+        </Button3D>
+
+        <Button3D>
+          <ButtonFace default>
+            <span>LIVE</span>
+          </ButtonFace>
+          <ButtonFace active>
+            <span>LIVE</span>
+          </ButtonFace>
+        </Button3D>
       </Wrap>
     );
   }
